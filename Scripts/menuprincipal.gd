8dd8892c96f1f4da4d9fc1d8b40ca892679daf4c
Ruddy Gil <ragil@ucundinamarca.edu.co>
2025-09-29 23:26:59 -0500

    Finalización menú y música
    Se completó el menú y se agregó música al videojuego

--- Scripts/menuprincipal.gd
+++ Scripts/menuprincipal.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var botones_menu: VBoxContainer = $BotonesMenu
+@onready var boton_options: Panel = $BotonOptions
+
+	
+func _ready() -> void:
+	botones_menu.visible = true
+	boton_options.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Nivel1.tscn")
@@ -7,7 +14,13 @@ func _on_start_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	print("Options presionado")
+	botones_menu.visible = false
+	boton_options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
