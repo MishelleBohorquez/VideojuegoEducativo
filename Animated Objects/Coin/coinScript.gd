1c5ef418106baaec8ea1669ea6eb8584ea6cce4f
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2025-09-30 19:13:54 -0500

    Limpieza profunda
    Se eliminan carpetas inncesarias

--- Animated Objects/Coin/coinScript.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("player detectado")
-		queue_free()
-	pass # Replace with function body.
