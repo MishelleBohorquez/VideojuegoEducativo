afda310066e33f43376067a7c4347646be59ec5c
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2025-10-28 16:38:59 -0500

    Mejora interfaz Nivel2
    Se mejora la interfaz del Nivel2 con plataformas y fuego.

---
+++ Scenes/fuego_animado.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play() # <-- ¡Esta es la línea que agregaste!
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
