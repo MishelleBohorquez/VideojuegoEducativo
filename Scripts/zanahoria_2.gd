c5ec38741731cbbf4fe2cacf2d762ba718293356
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2025-10-28 21:38:12 -0500

    Fuego del Nivel2, contador de zanahorias y zanahorias
    Se agrega el fuego en el nivel2, zanahorias y el contador

--- Scripts/zanahoria_2.gd
+++ Scripts/zanahoria_2.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-@onready var zanahoria_manger: Node = %ZanahoriaManger
+@onready var zanahoria_nivel2: Node = %ZanahoriaNivel2
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
-	zanahoria_manger.incrementa_un_punto()
+	zanahoria_nivel2.incrementa_un_punto()
 	queue_free()
